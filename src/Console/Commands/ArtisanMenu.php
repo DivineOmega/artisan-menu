c4fc2a128f3c6ed95b1148de816f1f33ef80f43a
Jordan Hall <jordan@hall05.co.uk>
2018-07-10 22:25:35 +0100

    Initial menu structure

--- src/Console/Commands/ArtisanMenu.php
+++ src/Console/Commands/ArtisanMenu.php
@@ -3,7 +3,9 @@
 namespace DivineOmega\ArtisanMenu\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Artisan;
+use PhpSchool\CliMenu\CliMenu;
 
 class ArtisanMenu extends Command
 {
@@ -38,8 +40,47 @@ class ArtisanMenu extends Command
      */
     public function handle()
     {
-        $commandList = json_decode(Artisan::call('list --format=json'));
+        $returnCode = Artisan::call('list', ['format' => 'json');
 
-        var_dump($commandList);
+        if ($returnCode != 0) {
+            $this->error('Error calling `artisan list` command.');
+            exit();
+        }
+
+        $response = json_decode(Artisan::output());
+
+        if (!$response) {
+            $this->error('Error decoding `artisan list` response.');
+            exit();
+        }
+
+        $this->app= new Collection($response->application);
+        $this->commands = new Collection($response->commands);
+        $this->namespaces = new Collection($response->namespaces);
+
+        $this->mainMenu();
+
+    }
+
+    private function mainMenu()
+    {
+
+        $menu = new CliMenuBuilder();
+        $menu->setTitle('Artisan Menu - '.this->app->name.' '.$this->app->version);
+        $menu->addLineBreak('-')
+        $menu->setBorder(1, 2, 'yellow')
+        $menu->setPadding(2, 4)
+        $menu->setMarginAuto()
+
+        foreach($this->namespaces as $namespace) {
+            $menu->addItem($namespace->id, ['this', 'namespaceMenu']);
+        }
+
+        $menu->build();
+    }
+
+    private function namespaceSelected(CliMenu $menu)
+    {
+        $selectedNamespace = $menu->getSelectedItem()->getText();
     }
 }
