27760354a04076f0024c18c4941f99513af743f4
Jordan Hall <jordan@langleyfoxall.co.uk>
2018-07-10 12:49:53 +0100

    Initial command class and composer file

---
+++ src/Console/Commands/ArtisanMenu.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DivineOmega\ArtisanMenu\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class SendEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'menu';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Use Artisan via a beautiful console GUI';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $commandList = json_decode(Artisan::call('list --format=json'));
+
+        var_dump($commandList);
+    }
+}
